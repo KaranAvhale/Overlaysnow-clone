921c8ce3a2642374ff0cc821e93841dcc46e59bf
Yashkpatil-19 <yashpatil2006@gmail.com>
2025-10-30 00:58:52 +0530

    Add to cart :-quantity(-,+)

--- Overlays/carts/views.py
+++ Overlays/carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
@@ -14,7 +14,7 @@ def cart_id(request):
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
-        cart = Cart.objects.get(cart_id=cart_id(request))  # get cart using the cart_id
+        cart = Cart.objects.get(cart_id=cart_id(request))  
     except Cart.DoesNotExist:
         cart = Cart.objects.create(cart_id=cart_id(request))
 
@@ -31,6 +31,17 @@ def add_cart(request, product_id):
         )
         cart_item.save()
      
+    return redirect('cart')
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=cart_id(request))
+    product = get_object_or_404(Product, id= product_id)
+    cart_item = CartItem.objects.get(product = product, cart =cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
     return redirect('cart')
 
 def cart(request):
