a74cbf8ff88c5e94d8834ec96cdbc90a99ed20ba
Yashkpatil-19 <yashpatil2006@gmail.com>
2025-10-30 00:00:22 +0530

    Add To cart :- product_image , product_name

--- Overlays/carts/urls.py
+++ Overlays/carts/urls.py
@@ -3,4 +3,7 @@ from  . import views
 
 urlpatterns = [
     path('' , views.cart, name='cart'),
+     path('add_car/<int:product_id>/',views.add_cart, name='add_cart'),
+
+
 ]
--- Overlays/carts/views.py
+++ Overlays/carts/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from store.models import Product
+from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+# Add product to cart
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=cart_id(request))  # get cart using the cart_id
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=cart_id(request))
+
+    cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+        cart_item.save()
+     
+    return redirect('cart')
+
 def cart(request):
-    return render(request, 'store/cart.html')
+    # Initialize variables
+    total = 0
+    quantity = 0
+    cart_items = []
+
+    try:
+        cart_obj = Cart.objects.get(cart_id=cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart_obj, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+    except Cart.DoesNotExist:
+        # Cart does not exist; cart_items already initialized as empty list
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+    }
+    return render(request, 'store/cart.html', context)
