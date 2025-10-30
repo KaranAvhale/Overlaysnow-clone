5a17a0ed501b4f1d2174d19816b0e4b24b755de5
Karan-Avhale <karan.code.in@gmail.com>
2025-10-31 04:48:55 +0530

    Add Forgot Password Functionality

--- Overlays/accounts/urls.py
+++ Overlays/accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('dashboard/' , views.dashboard, name='dashboard'),
     path('' , views.dashboard, name='dashboard'),
 
-
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
 ]
