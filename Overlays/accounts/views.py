99376aa3f311a63fd75cd861bb999e81181a4618
Karan-Avhale <karan.code.in@gmail.com>
2025-10-31 02:08:36 +0530

    Add Login Functionality

--- Overlays/accounts/views.py
+++ Overlays/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -22,6 +24,8 @@ def register(request):
             )
             user.phone_number = phone_number
             user.save()
+            messages.success(request, "Registration successful.")
+            return redirect('register')
             # Redirect to a success page or login page
     else:
         form = RegistrationForm()
@@ -31,7 +35,22 @@ def register(request):
     return render(request, 'accounts/register.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid Login Credentials!")
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
+@login_required(login_url='login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('login')
